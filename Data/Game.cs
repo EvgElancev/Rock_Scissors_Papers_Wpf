807c8fd934712d8e78cf08cde6ca27002194739f
gelij22 <87936582+gelij22@users.noreply.github.com>
2024-12-21 13:18:51 +0700

    Добавлена логика выбора игрока и компьютера

---
+++ Data/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Scissors_Papers_Wpf.Data
+{
+    internal class Game
+    {
+        public string? playerChoice { get; set; }
+        public string? ComputerCoice { get; set; }
+
+        private string[] choicesPcArr = [ "Rock", "Paper", "Scissors" ];
+        
+        public Uri? choicePCUri { get; set; }
+
+        public bool status = false;
+        
+        public Game(Uri uri)        
+        {          
+            choicePCUri = uri;          
+        }
+
+        public void ComputerCoiceGenerate()
+        {         
+           Random random = new Random();
+            ComputerCoice = choicesPcArr[random.Next(0, choicesPcArr.Length)];
+            SetImage(ComputerCoice);
+        }
+
+        private void SetImage(string choice)
+        {
+            if(choice  == "Rock")
+            {
+                choicePCUri = new Uri("/Assets/Rock.png", UriKind.Relative);
+                return;
+            }
+            if(choice == "Paper")
+            {
+                choicePCUri = new Uri("/Assets/Paper.jpg", UriKind.Relative);
+            }
+            if(choice == "Scissors")
+            {
+                choicePCUri = new Uri("/Assets/Scissors.jpg", UriKind.Relative);
+            }
+            
+            
+        }
+    }
+}
---
+++ Data/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Scissors_Papers_Wpf.Data
+{
+    public class Player
+    {
+        public string? Name { get; set; }   
+        public string? choice { get; set; }
+
+        public Uri? choiceUri { get; set; }  
+        public bool status = false;
+
+
+        public Player(Uri uri) 
+        { choiceUri = uri; }
+
+    }
+}
