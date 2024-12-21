8cccd9666b28f7de10ffe3a55182eed84cca6e44
gelij22 <87936582+gelij22@users.noreply.github.com>
2024-12-21 12:04:16 +0700

    Create_Interfase

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,28 @@ namespace Rock_Scissors_Papers_Wpf
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void RockBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Uri resourseUri = new Uri("/Assets/Rock.png", UriKind.Relative);           
+            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
+
+        }
+
+        private void ScissorsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Uri resourseUri = new Uri("/Assets/Scissors.jpg", UriKind.Relative);
+            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
+        }
+
+        private void PaperBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Uri resourseUri = new Uri("/Assets/Paper.jpg", UriKind.Relative);
+            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
         }
     }
 }
