ec254ab95b12f1a41a71a87e7c4b807b03e7104e
gelij22 <87936582+gelij22@users.noreply.github.com>
2024-12-21 13:15:12 +0700

    Добавлена логика выбора игрока и компьютера

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Rock_Scissors_Papers_Wpf.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,34 +18,61 @@ namespace Rock_Scissors_Papers_Wpf
     public partial class MainWindow : Window
     {
 
-        private bool session = false;
+        private Player newPlayer = new Player(new Uri("/Assets/default.jpg", UriKind.Relative));
+        Game newGame = new Game(new Uri("/Assets/default.jpg", UriKind.Relative));
+        //private string[] coicesPcArr = ["Rock", "Paper", "Scissors"];
+       
 
-        
-        
         public MainWindow()
         {
             InitializeComponent();
-
+            
+            ChoiseImgPlayer.Source = new BitmapImage(newPlayer.choiceUri);
+            ChoiseImgUI.Source = new BitmapImage(newGame.choicePCUri);
+            
+            //Statistica.Text = newGame.ComputerCoice;
         }
 
         private void RockBtn_Click(object sender, RoutedEventArgs e)
         {
+            newPlayer.choice = "Rock";
+            newPlayer.choiceUri = new Uri("/Assets/Rock.png", UriKind.Relative);
+            ChoiseImgPlayer.Source = new  BitmapImage(newPlayer.choiceUri);
 
-            Uri resourseUri = new Uri("/Assets/Rock.png", UriKind.Relative);           
-            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
+            newGame.ComputerCoiceGenerate();
+            ChoiseImgUI.Source = new BitmapImage(newGame.choicePCUri);
+        
+
 
         }
 
         private void ScissorsBtn_Click(object sender, RoutedEventArgs e)
         {
-            Uri resourseUri = new Uri("/Assets/Scissors.jpg", UriKind.Relative);
-            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
+            newPlayer.choice = "Scissors";
+            newPlayer.choiceUri = new Uri("/Assets/Scissors.jpg", UriKind.Relative);
+            ChoiseImgPlayer.Source = new BitmapImage(newPlayer.choiceUri);
+
+            newGame.ComputerCoiceGenerate();
+            ChoiseImgUI.Source = new BitmapImage(newGame.choicePCUri);
         }
 
         private void PaperBtn_Click(object sender, RoutedEventArgs e)
         {
-            Uri resourseUri = new Uri("/Assets/Paper.jpg", UriKind.Relative);
-            ChoiseImgPlayer.Source = new BitmapImage(resourseUri);
+            newPlayer.choice = "Paper";
+            newPlayer.choiceUri = new Uri("/Assets/Paper.jpg", UriKind.Relative);
+            ChoiseImgPlayer.Source = new BitmapImage(newPlayer.choiceUri);
+
+            newGame.ComputerCoiceGenerate();
+            ChoiseImgUI.Source = new BitmapImage(newGame.choicePCUri);
         }
+
+       private void GameStart (string playerChoise, string computerChoise)
+        {
+            //Отображается картинка выбора компьютера
+
+            //Результат игры
+        }
+
+       
     }
 }
